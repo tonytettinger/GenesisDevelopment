c68b7038c375bf5c9a69d59c52d8a43d9b9aa170
feelinglucky <antal.tettinger@clovio.com>
2019-04-06 00:05:25 +0800

    One half, first column practice finished

--- themes/genesis-sample/home.php
+++ themes/genesis-sample/home.php
@@ -6,8 +6,19 @@ add_filter('genesis_attr_entry', __NAMESPACE__ .  '\add_grid_classes_to_entry');
 
 function add_grid_classes_to_entry(array $attributes) {
     $attributes['class'] .= ' one-half';
+    $attributes['class'] .= get_first_column_class(); 
 
     return $attributes;
 }
 
-genesis();
+function get_first_column_class($number_of_columns = 2) {
+    static $column_count = 0;
+
+    $remainder = $column_count % $number_of_columns;
+    $column_count++;
+    if( $remainder == 0) {
+        return ' first';
+    }
+}
+
+genesis(); 
